27e4887298d0378a31cf88068cc35dc32daf131c
fatihstl <fatih11iyiyol@gmail.com>
2020-10-31 13:39:09 -0500

    Bakalim simdi ne olacak

--- src/main/java/featureACode.java
+++ src/main/java/featureACode.java
@@ -2,6 +2,6 @@ public class featureACode {
     public static void main(String[] args) {
 
 
-        System.out.println("Sihirbaz cocuk");
+        System.out.println("Sihirbaz cocuk, merhana");
     }
 }
